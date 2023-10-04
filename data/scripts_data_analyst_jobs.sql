821ecdfb6c0056e047a33b156dde7c609049ec09
antoinetteanderson <antoinetteanderson88@gmail.com>
2023-10-03 21:51:13 -0500

    finished question 6

--- data/scripts_data_analyst_jobs.sql
+++ data/scripts_data_analyst_jobs.sql
@@ -33,9 +33,12 @@ WHERE review_count BETWEEN 500 AND 1000;
 -- 151 postings
 
 -- 6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
-SELECT AVG(star_rating) AS avg_rating, location
+SELECT AVG(star_rating) AS avg_rating, location AS state
 FROM data_analyst_jobs
-GROUP BY location;
+GROUP BY location
+ORDER BY avg_rating DESC; 
+
+-- Nebraska has the highest average star rating.
 
 -- 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
 
